99f6670c87af1bd584b5d5d06132e70b8417325a
clementpaul112 <72744246+clementpaul112@users.noreply.github.com>
2023-07-20 21:06:42 +0530

    Add files via upload

---
+++ sharpner.js
@@ -0,0 +1,30 @@
+Status = "";
+sharpner_image = "";
+
+function preload()
+{
+    battery_image = loadImage("sharpner.jpg");
+}
+
+function setup()
+{
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_detector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    Status = true;
+    object_detector.detect(sharpner_image,gotResults); 
+}
+
+function gotResults(results,error)
+{
+    if (error){
+        console.error(error);
+    }
+    console.log(results);
+}
